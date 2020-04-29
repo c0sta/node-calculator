93ec8ab8679cf6096c93c7a2a7f9d8475694c963
Gabriel <gabrielcostamoura1@gmail.com>
2020-04-29 12:26:38 -0300

    :art: Adding cors support

--- app.js
+++ app.js
@@ -6,13 +6,26 @@ var express = require("express"),
 
 var server = express();
 
+var whitelist = [
+  "https://node-calculator.herokuapp.com",
+  "http://localhost:8080",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 // Configura os middlewares
 server.use(morgan("dev")); // Middleware que auxilia com o debug do app
 server.use(routes); // Middleware das rotas
 server.use(express.static("./src/public")); // Servindo os arquivos estáticos, html, css.
-server.use(cors());
+server.use(cors(corsOptions));
 
 // Roda servidor na porta 8080
 server.listen(8080, function () {
-  console.log("==== SERVER LISTENING IN 8080 ====");
+  console.log("==== SERVER LISTENING ON 8080 ====");
 });
