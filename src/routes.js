8f99620b4ef02d5a4a3c20cc7429db768e613155
Gabriel <gabrielcostamoura1@gmail.com>
2020-04-29 11:02:35 -0300

    :sparkles: Added routes to see logs in /log

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-// var path = require("path");
+// Controllers
 var OperationsController = require("./controllers/operacoes");
 var MainController = require("./controllers/main");
-
-var jsonParser = bodyParser.json(),
-  urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+var LogController = require("./controllers/log");
+// Uso a função Router do express, que nos permite criar as rotas
 var routes = express.Router();
+// Esse parser é necessário quando vamos fazer métodos
+// post's no express
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// Monitora as requisições no path raíz "/"
+// Monitora as requisições de GET no caminho raíz "/"
 routes.get("/", MainController.main);
+routes.get("/log", LogController.log);
+/* Monitora as requisições de POST nos caminhos de cada operação
+  OBS - Passar esse urlencodedParser foi o que fez funcionar,
+  até então os números inseridos estavam vindo como undefined 
+  */
 routes.post("/sum", urlencodedParser, OperationsController.sum);
 routes.post("/subtraction", urlencodedParser, OperationsController.subtraction);
 routes.post(
