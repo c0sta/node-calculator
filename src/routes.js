e6ecf1210a06c6ce25a8234c2702d08a848e9846
Gabriel <gabrielcostamoura1@gmail.com>
2020-04-29 18:16:18 -0300

    :art: Removed single route per operation
    Added a single route /calculate that deals with all operations

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 // Controllers
-var OperationsController = require("./controllers/operacoes");
-var MainController = require("./controllers/main");
-var LogController = require("./controllers/log");
+var OperationsController = require("./controllers/operacoes-controller");
+var MainController = require("./controllers/main-controller");
+var LogController = require("./controllers/log-controller");
 // Uso a função Router do express, que nos permite criar as rotas
 var routes = express.Router();
 // Esse parser é necessário quando vamos fazer métodos
@@ -13,17 +13,10 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 // Monitora as requisições de GET no caminho raíz "/"
 routes.get("/", MainController.main);
 routes.get("/log", LogController.log);
-/* Monitora as requisições de POST nos caminhos de cada operação
+/* Monitora as requisições de POST na rota /calculate
   OBS - Passar esse urlencodedParser foi o que fez funcionar,
   até então os números inseridos estavam vindo como undefined 
   */
-routes.post("/sum", urlencodedParser, OperationsController.sum);
-routes.post("/subtraction", urlencodedParser, OperationsController.subtraction);
-routes.post(
-  "/multiplication",
-  urlencodedParser,
-  OperationsController.multiplication
-);
-routes.post("/division", urlencodedParser, OperationsController.division);
+ routes.post('/calculate', urlencodedParser,OperationsController.calculate)
 
 module.exports = routes;
